9a4a0c2217821172ba7915010f190bd697c85b46
g_babich <aeksex1@gmail.com>
2024-02-14 21:22:34 +0100

    - Access modifiers fix for ShiMemberProfile

--- Assets/_Grisha/Scripts/ShipMemberProfile.cs
+++ Assets/_Grisha/Scripts/ShipMemberProfile.cs
@@ -5,30 +5,21 @@ using UnityEngine;
 public class ShipMemberProfile
 {
     [Header("Member data")]
-    [SerializeField]
-    int memberHeight;
-    [SerializeField]
-    int memberWeight;
-    [SerializeField]
-    ShipMemberSex shipMemberSex = ShipMemberSex.male;
-    [SerializeField]
-    int memberAge;
-    [SerializeField] 
-    int bloodType = 0;
+    public int memberHeight;
+    public int memberWeight;
+    public ShipMemberSex shipMemberSex;
+    public int memberAge;
+    public int bloodType;
     [Header(header: "Medical indicators")]
-    [SerializeField]
-    int heartBeat;
-    [SerializeField]
-    BloodPressure bloodPressure;    
+    public int heartBeat;
+    public BloodPressure bloodPressure;    
 }
 
 public enum ShipMemberSex {male, female}
 [Serializable]
 public class BloodPressure
 {
-    [SerializeField]
-    int upper;
-    [SerializeField]
-    int bottom;
+    public int upper;
+    public int bottom;
 }
 
