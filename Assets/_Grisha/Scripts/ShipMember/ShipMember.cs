74387f22789242d9e5b5cd8c8263d12a3745b6b0
IRKhabibullin <irkhabibullin@gmail.com>
2024-02-15 18:56:01 +0100

    xRay body parts and xRay itself

--- Assets/_Grisha/Scripts/ShipMember/ShipMember.cs
+++ Assets/_Grisha/Scripts/ShipMember/ShipMember.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShipMember : MonoBehaviour
@@ -8,30 +6,77 @@ public class ShipMember : MonoBehaviour
     [SerializeField] SO_ShipMemberProfile shipMemberProfile;
 
     [Header("Body parts")]
-    public SO_BodyPart Head;
-    public SO_BodyPart Torso;
-    public SO_BodyPart RightHand;
-    public SO_BodyPart LeftHand;
-    public SO_BodyPart RightLeg;
-    public SO_BodyPart LeftLeg;
-    Dictionary<string, SO_BodyPart> soBodyParts;
+    [SerializeField] private SO_BodyPart Head;
+    [SerializeField] private SO_BodyPart Torso;
+    [SerializeField] private SO_BodyPart RightHand;
+    [SerializeField] private SO_BodyPart LeftHand;
+    [SerializeField] private SO_BodyPart RightLeg;
+    [SerializeField] private SO_BodyPart LeftLeg;
+
+    [Header("Altered body parts")]
+    public List<BodyPart> damagedBodyParts;
+    public BodyPart infectedBodyPart;
+
+    [Header("Body parts sprites")]
+    [SerializeField] private SpriteRenderer headSuit;
+    [SerializeField] private SpriteRenderer torsoSuit;
+    [SerializeField] private SpriteRenderer rightHandSuit;
+    [SerializeField] private SpriteRenderer leftHandSuit;
+    [SerializeField] private SpriteRenderer rightLegSuit;
+    [SerializeField] private SpriteRenderer leftLegSuit;
+    
+    [SerializeField] private SpriteRenderer headXRay;
+    [SerializeField] private SpriteRenderer torsoXRay;
+    [SerializeField] private SpriteRenderer rightHandXRay;
+    [SerializeField] private SpriteRenderer leftHandXRay;
+    [SerializeField] private SpriteRenderer rightLegXRay;
+    [SerializeField] private SpriteRenderer leftLegXRay;
+    
+    
+    private Dictionary<BodyPart, (SO_BodyPart, SpriteRenderer, SpriteRenderer)> _bodyPartsMap;
     void Start()
     {
-        soBodyParts = new() 
+        _bodyPartsMap = new() 
         {
-            {"Head", Head}, {"Torso", Torso}, 
-            {"RightHand", RightHand}, {"LeftHand", LeftHand},
-            {"RightLeg", RightLeg}, {"LeftLeg", LeftLeg},
+            {BodyPart.Head, (Head, headSuit, headXRay)},
+            {BodyPart.Torso, (Torso, torsoSuit, torsoXRay)}, 
+            {BodyPart.RightHand, (RightHand, rightHandSuit, rightHandXRay)},
+            {BodyPart.LeftHand, (Head, leftHandSuit, leftHandXRay)},
+            {BodyPart.RightLeg, (RightLeg, rightLegSuit, rightLegXRay)},
+            {BodyPart.LeftLeg, (Head, leftLegSuit, leftLegXRay)},
         };
     }
+
     /// <summary>
-    /// Применяет состояние для части тела
+    /// Перерисовываем все части тела с учетом ран и заражения личинкой
     /// </summary>
-    /// <param name="bodyPartName"> Название части тела (Head, Torso, RightHand, LeftHand, RightLeg, LeftLeg) </param>
-    /// <param name="bodyPartState"> Название  состояния части тела (healty, damaged, xRay, xRayInfected) </param>
-    public void SetBodyPartState(string bodyPartName, string bodyPartStateName)
+    public void RedrawBodyParts()
     {
-        var _soBodyPart = soBodyParts[bodyPartName].GetSpriteByState(bodyPartStateName);
-        gameObject.transform.Find(bodyPartName).GetComponent<SpriteRenderer>().sprite = _soBodyPart;
+        foreach (var (bodyPart, (so, suitPart, xRayPart)) in _bodyPartsMap)
+        {
+            suitPart.sprite = so.healthy;
+            xRayPart.sprite = so.xRay;
+
+            if (damagedBodyParts.Contains(bodyPart))
+            {
+                suitPart.sprite = so.damaged;
+            }
+
+            if (infectedBodyPart == bodyPart)
+            {
+                xRayPart.sprite = so.xRayInfected;
+            }
+        }
     }
+}
+
+public enum BodyPart
+{
+    None,
+    Head,
+    Torso,
+    RightHand,
+    LeftHand,
+    RightLeg,
+    LeftLeg
 }
