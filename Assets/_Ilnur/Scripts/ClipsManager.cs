dfaba474af2ba74812ad29d085c8714cfb18d668
IRKhabibullin <irkhabibullin@gmail.com>
2024-02-17 17:42:58 +0100

    clip manager fix

--- Assets/_Ilnur/Scripts/ClipsManager.cs
+++ Assets/_Ilnur/Scripts/ClipsManager.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class ClipsManager : MonoBehaviour
 {
-    private Animator _animator;
+    [SerializeField] private Animator _animator;
 
     #region Intro
     private void RunIntro()
     {
+        _animator.gameObject.SetActive(true);
         _animator.SetTrigger("RunIntro");
     }
 
     private void FinishIntro()
     {
+        _animator.gameObject.SetActive(false);
         GameEventsBus.ShipMembersGoingGathering?.Invoke();
     }
     #endregion
@@ -19,11 +21,13 @@ public class ClipsManager : MonoBehaviour
     #region RunInfectedShipMember
     private void RunLettingInfectedShipMemberIn()
     {
+        _animator.gameObject.SetActive(true);
         _animator.SetTrigger("RunInfectedShipMember");
     }
 
     private void FinishLettingInfectedShipMemberIn()
     {
+        _animator.gameObject.SetActive(false);
         PlanetEventsBus.ShipMemberComingToShip?.Invoke();
     }
     #endregion
@@ -31,19 +35,16 @@ public class ClipsManager : MonoBehaviour
     #region RunLettingShipMemberIn
     private void RunLettingShipMemberIn()
     {
+        _animator.gameObject.SetActive(true);
         _animator.SetTrigger("RunLettingShipMemberIn");
     }
 
     private void FinishLettingShipMemberIn()
     {
+        _animator.gameObject.SetActive(false);
         PlanetEventsBus.ShipMemberComingToShip?.Invoke();
     }
     #endregion
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
 
     private void OnEnable()
     {
